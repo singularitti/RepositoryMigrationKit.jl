68c0d955eb136b99b046b6f80fcb5b1e418497fc
singularitti <singularitti@outlook.com>
2023-08-01 19:36:23 -0400

    Define `get_head_sha` function for retrieving SHA of head commit in repository

--- src/RepositoryMigrationKit.jl
+++ src/RepositoryMigrationKit.jl
@@ -1,5 +1,13 @@
 module RepositoryMigrationKit
 
-# Write your package code here.
+using LibGit2: GitRepo, head, GitHash
+
+export get_head_sha
+
+function get_head_sha(repo_path)
+    repo = GitRepo(repo_path)
+    head_commit = head(repo)
+    return GitHash(head_commit)
+end
 
 end
